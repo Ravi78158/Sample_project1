7771e57cad18d7b1b1fe3498487ba6dfafb587fb
Ravi78158 <sativadaravikumar44@gmail.com>
2025-07-03 22:58:30 +0530

    @practice

---
+++ Num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Dum
+{
+public static void main(String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a value is:");
+int a=sc.nextInt();
+System.out.println("enter b value is:");
+int b=sc.nextInt();
+System.out.println("enter c value is:");
+int c=sc.nextInt();
+int x=a+b+c;
+System.out.println(" the sum of three numbers is:"+x);
+ }
+ }
