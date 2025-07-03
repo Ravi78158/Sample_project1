39775890f0b8199e23e415a804fddedfcabbe90b
Ravi78158 <sativadaravikumar44@gmailo.com>
2025-07-03 22:28:13 +0530

    practice

---
+++ Marray1.java
@@ -0,0 +1,30 @@
+class Marray1
+{
+public static void main(String[] args)
+{
+int i[][]=new int[4][4];
+System.out.println(i);
+System.out.println(i.length);
+System.out.println(i[0]);
+System.out.println(i[0][0]);
+System.out.println(i[0][1]);
+System.out.println(i[0][2]);
+System.out.println(i[0][3]);
+System.out.println(i[1]);
+System.out.println(i[1][0]);
+System.out.println(i[1][1]);
+System.out.println(i[1][2]);
+System.out.println(i[1][3]);
+System.out.println(i[2]);
+System.out.println(i[2][0]);
+System.out.println(i[2][1]);
+System.out.println(i[2][2]);
+System.out.println(i[2][3]);
+System.out.println(i[3]);
+System.out.println(i[3][0]);
+System.out.println(i[3][1]);
+System.out.println(i[3][2]);
+System.out.println(i[3][3]);
+
+}
+}
