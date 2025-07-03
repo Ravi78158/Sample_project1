1d64f16997d3bb63349a27434f2a5a31016e9329
Ravi78158 <sativadaravikumar44@gmailo.com>
2025-07-03 21:57:18 +0530

    Marray.java

---
+++ Marray.java
@@ -0,0 +1,14 @@
+class Marray
+{
+public static void main(String[] args)
+{
+int []i={1,3,4,5,6};
+System.out.println(i);
+System.out.println(i.length);
+System.out.println(i[0]);
+System.out.println(i[1]);
+System.out.println(i[2]);
+System.out.println(i[3]);
+System.out.println(i[4]);
+}
+}
